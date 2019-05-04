bf5811b6448c3d5bd18d6b016ad48ff2e9f5324b
Gar <a.garnica@alumnos.upm.es>
2019-05-04 18:52:49 +0200

    Citas de pacientes y confirmar presencia

--- src/app/appointments/appointments.component.ts
+++ src/app/appointments/appointments.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AppointmentsService} from './appointments.service';
+
 
 @Component({
   selector: 'app-appointments',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppointmentsComponent implements OnInit {
 
-  constructor() { }
+  appointments;
+  time = [
+    "9:00-9:30",
+    "9:30-10:00",
+    "10:00-10:30",
+    "10:30-11:00",
+    "11:00-11:30",
+    "11:30-12:00",
+    "12:00-12:30",
+    "12:30-13:00",
+    "13:00-13:30",
+    "13:30-14:00"
+  ];
+  constructor(public appointmentsService: AppointmentsService) { }
+
 
   ngOnInit() {
+    this.appointmentsService.getAppointments().subscribe(response=>{
+      this.appointments= response;
+      console.log(this.appointments);
+    });
+  }
+
+  confirmClick(id){
+    this.appointmentsService.confirmPresence(id);
+    var first = true;
+    setInterval(() => {
+      if(first){
+        this.ngOnInit();
+        first = false;
+      }
+
+     }, 1000);
   }
 
 }
---
+++ src/app/appointments/appointments.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders  } from "@angular/common/http";
+
+@Injectable({ providedIn: "root" })
+export class AppointmentsService {
+    
+    constructor(private http: HttpClient) {}
+
+    
+
+    getAppointments(){
+        return this.http.get("http://localhost:8080/ISST2019/api/pat-appointment?dni=22222222");
+    }
+    confirmPresence(id){
+        const httpOptions = {
+            headers: new HttpHeaders({
+                'Content-Type':'application/json; charset=utf-8'
+            })
+        };
+        return this.http.post("http://localhost:8080/ISST2019/api/presence?id="+id,httpOptions)
+        .subscribe(
+            res => {
+              console.log(res);
+            },
+            err => {
+                console.log(err);
+            console.log("Error occured");
+            }
+          );
+    }
+
+}
